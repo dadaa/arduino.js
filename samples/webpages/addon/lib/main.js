8beca4176d314e2393d01d8faf0100ee1619d94a
Daisuke Akatsuka <dadaaism@gmail.com>
2012-04-24 10:04:37 +0900

    adds LICENSE

--- samples/webpages/addon/lib/main.js
+++ samples/webpages/addon/lib/main.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 const xulApp = require("api-utils/xul-app");
 const HAS_DOCUMENT_ELEMENT_INSERTED =
         xulApp.versionInRange(xulApp.platformVersion, "2.0b6", "*");
